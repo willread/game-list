8483cc238d369a8ebe40f5b1ce80f7ad508d91a8
Will Read <will@willread.ca>
2020-05-07 18:50:14 -0600

    Fixed incorrect time rendering after 24 hours

--- src/app/pipes/seconds-to-time.pipe.ts
+++ src/app/pipes/seconds-to-time.pipe.ts
@@ -11,7 +11,10 @@ export class SecondsToTimePipe implements PipeTransform {
   ) {}
 
   transform(seconds: number): string {
-    return (new Date(seconds * 1000)).toUTCString().match(/(\d\d:\d\d:\d\d)/)[0];
+    const hh = Math.floor(seconds / 3600) < 10 ? ("00" + Math.floor(seconds / 3600)).slice(-2) : Math.floor(seconds / 3600);
+    const mm = ("00" + Math.floor((seconds % 3600) / 60)).slice(-2);
+    const ss = ("00" + (seconds % 3600) % 60).slice(-2);
+    return hh + ":" + mm + ":" + ss;
   }
 
 }
