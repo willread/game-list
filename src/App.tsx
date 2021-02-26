35b0295580731b99bbbc12d36cabaeab7822df1b
Will Read <will@willread.ca>
2021-02-26 13:01:07 -0700

    Add basic user registration / login

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider } from '@apollo/client';
+import React, { useContext, useEffect } from 'react';
+import { ApolloClient, createHttpLink, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { relayStylePagination } from "@apollo/client/utilities";
 
@@ -9,18 +9,36 @@ import SignUpForm from './components/SignUpForm';
 import Games from './Games';
 import './App.css';
 
+const FETCH_USER = gql`
+  query viewer {
+    viewer {
+      sessionToken
+      user {
+        username
+        email
+      }
+    }
+  }
+`;
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:3004/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('token');
+  const newHeaders: { [key: string]: string } = {
+    'X-Parse-Application-Id': 'myAppId',
+  };
 
+  if (token) {
+    newHeaders['X-Parse-Session-Token'] = token;
+  }
+  
   return {
     headers: {
       ...headers,
-      'X-Parse-Application-Id': 'myAppId',
-      // 'X-Parse-Session-Token': token,
+      ...newHeaders,
     }
   }
 });
@@ -41,6 +59,21 @@ const client = new ApolloClient({
 
 function App() {
   const { state } = useContext(storeContext);
+
+  useEffect(() => {
+    useQuery(FETCH_USER, {
+      onCompleted: data => {
+        const token = data.viewer.sessionToken;
+  
+        localStorage.setItem('token', token); // TODO: Move to action
+        console.log('fethced user', data):
+        // dispatch({
+        //   type: 'SET_USER',
+        //   payload: data.viewer.user,
+        // });
+      },
+    });
+  }, []);
 
   return (
     <ApolloProvider client={client}>
