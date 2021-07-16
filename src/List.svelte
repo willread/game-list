fc0df50f9a77e15e00b36ea62513088788bcb387
Will Read <will@willread.ca>
2021-07-16 14:50:52 -0600

    Add basic list management

---
+++ src/List.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { createEventDispatcher } from 'svelte';
+
+  export let list;
+
+  const dispatch = createEventDispatcher();
+
+  function remove() {
+		dispatch('remove', { id: list.id });
+	}
+
+  function update() {
+    dispatch('update', list);
+  }
+</script>
+
+<input bind:value={list.name}>
+<button on:click={update}>Update</button>
+<button on:click={remove}>X</button>
--- src/Lists.svelte
+++ src/Lists.svelte
@@ -1,18 +1,46 @@
 <script>
   import { db } from './firebase';
+  import firebase from 'firebase/app';
   import { collectionData } from 'rxfire/firestore';
   import { startWith } from 'rxjs/operators';
 
+  import List from './List.svelte';
+
   export let uid;
+
+  function removeList(event) {
+    const { id } = event.detail;
+
+    db.collection('lists').doc(id).delete();
+  }
+
+  function updateList(event) {
+    const { id, ...data } = event.detail;
+
+    db.collection('lists').doc(id).update(data);
+  }
+
+  function addList() {
+    db.collection('lists').add({ uid, name: newListName, created: firebase.firestore.FieldValue.serverTimestamp() });
+    newListName = '';
+  }
 
   const query = db.collection('lists').where('uid', '==', uid).orderBy('created');
   const lists = collectionData(query, 'id').pipe(startWith([]));
+  let newListName = '';
 </script>
 
 <h1>My Lists ({$lists.length})</h1>
-<h1>{uid}</h1>
 <ul>
 	{#each $lists as list}
-    {list.name}
+    <li>
+      {list.name}:
+      <List list={{...list}} on:remove={removeList} on:update={updateList} />
+    </li>
 	{/each}
 </ul>
+
+<h1>New List</h1>
+
+<input bind:value={newListName} placeholder="Name">
+<button on:click={addList}>Add</button>
