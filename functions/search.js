d10c44935d2c0409e20a37311dda6a61ef331b00
Will Read <will@willread.ca>
2021-08-16 10:01:52 -0600

    Increase memory allocation of search function

--- functions/search.js
+++ functions/search.js
@@ -47,44 +47,47 @@ async function readSearchIndex(admin, functions) {
 
 exports.functions = (admin, functions) => {
   try {
-    const searchGames = functions.https.onCall(async data => {
-      try {
-        if (!SEARCH_INDEX) {
-          if (SEARCH_INDEX_READ_PROMISE) {
-            await SEARCH_INDEX_READ_PROMISE;
-          } else {
-            await readSearchIndex(admin, functions);
-          }
-        }
-
-        const query = (data.search || '').toLowerCase();
-
-        if (!query) {
-          throw new functions.https.HttpsError('not-found', 'todo');
-        } else {
-          const results = SEARCH_DOCUMENTS.search(query, MAX_MATCHES);
-
-          if (results) {
-            const nameResults = results.find(r => r.field === 'name');
-
-            if (nameResults && nameResults.result.length){
-              const games = nameResults.result.map(r => SEARCH_INDEX.find(i => i.id == r));
-
-              functions.logger.log(`Found ${games.length} matches for '${query}'`, games);
-
-              return games;
+    const searchGames = functions
+      .runWith({ memory: '1GB' })
+      .https
+      .onCall(async data => {
+        try {
+          if (!SEARCH_INDEX) {
+            if (SEARCH_INDEX_READ_PROMISE) {
+              await SEARCH_INDEX_READ_PROMISE;
             } else {
+              await readSearchIndex(admin, functions);
+            }
+          }
+
+          const query = (data.search || '').toLowerCase();
+
+          if (!query) {
+            throw new functions.https.HttpsError('not-found', 'todo');
+          } else {
+            const results = SEARCH_DOCUMENTS.search(query, MAX_MATCHES);
+
+            if (results) {
+              const nameResults = results.find(r => r.field === 'name');
+
+              if (nameResults && nameResults.result.length){
+                const games = nameResults.result.map(r => SEARCH_INDEX.find(i => i.id == r));
+
+                functions.logger.log(`Found ${games.length} matches for '${query}'`, games);
+
+                return games;
+              } else {
+                throw new functions.https.HttpsError('not-found', 'todo');
+              }
+            } else {
+              functions.logger.log(`No match for '${query}'`);
               throw new functions.https.HttpsError('not-found', 'todo');
             }
-          } else {
-            functions.logger.log(`No match for '${query}'`);
-            throw new functions.https.HttpsError('not-found', 'todo');
           }
+        } catch(e) {
+          throw e;
         }
-      } catch(e) {
-        throw e;
-      }
-    });
+      });
 
     return {
       searchGames,
