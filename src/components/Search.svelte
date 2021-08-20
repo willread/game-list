e2a598e14fb02a3c522d5aee117d33743f13ea50
Will Read <will@willread.ca>
2021-08-20 11:36:53 -0600

    Tweak search ux

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -2,11 +2,13 @@
   import { format, fromUnixTime } from 'date-fns';
   import { link } from 'svelte-routing';
 
+  import { clickOutside } from '../actions/clickOutside';
   import { functions } from '../firebase';
   import { gamesForIds } from '../lists';
 
   import Cover from './Cover.svelte';
 
+  let query;
   let results = [];
   let timer;
   let lastSearchTime = -Infinity;
@@ -29,7 +31,7 @@
 
       if ((new Date()).getTime() >= lastSearchTime) {
         results = newResults;
-        dropdownOpen = true;
+        openDropdown();
       }
     } catch(e) {
       results = [];
@@ -48,11 +50,33 @@
       }, DEBOUNCE_TIMEOUT);
     }
 	}
+
+  function openDropdown() {
+    if (resultsLoading || results.length) {
+      dropdownOpen = true;
+    }
+  }
+
+  function closeDropdown() {
+    dropdownOpen = false;
+  }
+
+  function handleClickResult() {
+    closeDropdown();
+    query = '';
+  }
 </script>
 
-<div class="dropdown {dropdownOpen ? 'is-active' : ''}">
+<style lang="scss">
+  .dropdown-content {
+    max-height: 200px;
+    overflow: auto;
+  }
+</style>
+
+<div class="dropdown {dropdownOpen ? 'is-active' : ''}" use:clickOutside on:click-outside={closeDropdown}>
   <div class="dropdown-trigger" aria-haspopup="true" aria-controls="dropdown-menu">
-    <input class="input" on:input={debounce} />
+    <input class="input" on:input={debounce} on:focus={openDropdown} bind:value={query} />
   </div>
   <nav class="dropdown-menu" role="menu">
     <div class="dropdown-content is-flex is-justify-content-center is-flex-direction-column">
@@ -62,7 +86,7 @@
         {#if results.length}
           {#each results as result}
             {#if $games[result.id]}
-              <a href='/games/{result.slug}' use:link class="dropdown-item is-flex is-align-items-center">
+              <a href='/games/{result.slug}' use:link on:click={handleClickResult} class="dropdown-item is-flex is-align-items-center">
                 <span class="mr-1">
                   <Cover game={$games[result.id]} size='micro' />
                 </span>
@@ -74,7 +98,7 @@
             {/if}
           {/each}
         {:else}
-            No results found
+          <span class="has-text-dark px-3">No results found</span>
         {/if}
       {/if}
     </div>
