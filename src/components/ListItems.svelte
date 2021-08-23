6a349a079512e6d7313e031297f1f978c1dc88fa
Will Read <will@willread.ca>
2021-08-23 10:03:39 -0600

    Add basic sorting

--- src/components/ListItems.svelte
+++ src/components/ListItems.svelte
@@ -1,25 +1,53 @@
 <script>
-  import { gamesForIds, listItemsForId, lists } from '../lists';
+  import { writable } from 'svelte/store';
+
+  import { gamesForIds, listItemsForId } from '../lists';
   import MoveToList from './MoveToList.svelte';
   import { platforms } from '../services/platforms';
 
   export let listId;
 
+  const sortOptions = [
+    {obj: 'game', key: 'name', ascending: true, label: 'Name'},
+    {obj: 'game', key: 'name', ascending: false, label: 'Name'},
+  ];
+  const defaultSortOption = localStorage.getItem('sortBy') || sortOptions[0];
+  let sortBy = new writable(sortOptions.find(o => JSON.stringify(defaultSortOption) === JSON.stringify(o)) || sortOptions[0]);
+  let sortedListItems = [];
+
   $: listItems = listItemsForId(listId);
   $: games = gamesForIds($listItems.map(i => i.gameId));
+  $: games.subscribe(() => sortedListItems = sort($listItems, $sortBy))
+
+  function sort(listItemsToSort, sortBy) {
+    return listItemsToSort.sort((a, b) => {
+      const objA = (sortBy.obj === 'game' ? $games[a.gameId] : a) || {};
+      const objB = (sortBy.obj === 'game' ? $games[b.gameId] : b) || {};
+      const diff = (objA[sortBy.key] || '').localeCompare((objB[sortBy.key] || ''));
+
+      return sortBy.ascending ? -diff : diff;
+    });
+  }
 
   function removeListItem(listItem) {
     listItems.remove(listItem.id);
   }
-
-  function moveListItem(listItemId, newListId) {
-    listItems.move(listItemId, newListId);
-  }
 </script>
 
+<div class="select">
+  <select bind:value={$sortBy}>
+    {#each sortOptions as sortOption}
+      <option value={sortOption}>
+        {sortOption.label}
+        {@html sortOption.ascending ? '&uarr;' : '&darr;'}
+      </option>
+    {/each}
+  </select>
+</div>
+
 <ul>
-  {#if $listItems}
-    {#each $listItems as listItem}
+  {#if sortedListItems}
+    {#each sortedListItems as listItem}
       <li>
         {listItem.gameId}:
         {#if $games && $games[listItem.gameId]}
